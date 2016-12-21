4b0b5dee62424679bee7be1d06a4f105e984eb1e
Ruairi Fahy <ruairifahy91@gmail.com>
2016-12-21 11:57:19 +0000

    Bump version: 0.13.0 → 0.13.1

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ if IS_PYPY:
 
 setup(
     name='scrapely',
-    version='0.13.0',
+    version='0.13.1',
     license='BSD',
     description='A pure-python HTML screen-scraping library',
     author='Scrapy project',
